fcb3878a83ccad5ae2f92df4f3f8b308dff16926
NevermindG <gonzaloorlando2001@gmail.com>
2022-05-15 23:12:35 -0500

    v1

--- src/main/java/com/max/taskmanagermax_api/utility/CreateRoles.java
+++ src/main/java/com/max/taskmanagermax_api/utility/CreateRoles.java
@@ -6,21 +6,21 @@ import com.max.taskmanagermax_api.service.RoleService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
-@Component
-public class CreateRoles implements CommandLineRunner {
-
-    private final RoleService roleService;
-
-    public CreateRoles(RoleService roleService) {
-        this.roleService = roleService;
-    }
-
-    @Override
-    public void run(String... args) throws Exception {
-        Role adminRole = new Role (RoleName.ROLE_ADMIN);
-        Role userRole= new Role (RoleName.ROLE_USER);
-        roleService.save(adminRole);
-        roleService.save(userRole);
-    }
-
-}
+//@Component
+//public class CreateRoles implements CommandLineRunner {
+//
+//    private final RoleService roleService;
+//
+//    public CreateRoles(RoleService roleService) {
+//        this.roleService = roleService;
+//    }
+//
+//    @Override
+//    public void run(String... args) throws Exception {
+//        Role adminRole = new Role (RoleName.ROLE_ADMIN);
+//        Role userRole= new Role (RoleName.ROLE_USER);
+//        roleService.save(adminRole);
+//        roleService.save(userRole);
+//    }
+//
+//}
